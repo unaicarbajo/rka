ad835a7048aff5c0ccbe4fe215ad4dc793abce2b
869211 <869211@U030153.gi>
2020-10-13 11:28:53 +0200

    Lauki alde metro 1 script

---
+++ workspace/lauki.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import OUTPUT_C, OUTPUT_B, MoveTank
+from time import sleep
+
+def forward(tank, v, tsec):
+    tank.on_for_seconds(v, v, seconds = tsec)
+
+def turnLeft(tank, vl, vr, tsec):
+    # turn left on the spot
+    tank.on_for_seconds(vl, vr, seconds = tsec)
+
+def main():
+    # The MoveTank class provides the simplest way to drive two motors
+    # # left motor D
+    # # right motor A
+    
+    robot = MoveTank(OUTPUT_C, OUTPUT_B)
+    for _ in range(0, 4):
+        forward(robot, 50, 4)
+        turnLeft(robot, 20, 0, 2)
+    print("Square finished")
+    
+if __name__=="__main__":
+    main()
