c1136eff02e15029679dd5726656917af3edb7f5
unaicarbajo <unaibasket23@gmail.com>
2020-11-19 21:33:04 +0100

    Build added

---
+++ mapa/build/CMakeFiles/playerMapping.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ehu/Desktop/rka/mapa/src/mapping.cc" "/home/ehu/Desktop/rka/mapa/build/CMakeFiles/playerMapping.dir/src/mapping.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/player-3.1"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mapa/build/CMakeFiles/playerMapping.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/playerMapping.dir/src/mapping.cc.o"
+  "playerMapping.pdb"
+  "playerMapping"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/playerMapping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
